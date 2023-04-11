5beffcb0f1204f80e8fc42426a315ec55551ad78
cristian <nanonathan.123@gmail.com>
2023-04-11 16:43:42 -0300

    agrego lógica para crear threads

--- TP2/E01.c
+++ TP2/E01.c
@@ -1,15 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <pthread.h>
 
-int main(int count, char *parameters[])
-{
+void executeType1 () {
+    printf("Type 1.");    
+}
+
+void executeType2 () {
+    printf("Type 2.");
+}
+
+int main(int count, char *parameters[]) {
     int N, M;
     if(count != 2){
         N = atoi(parameters[0]);
         M = atoi(parameters[0]);
-        if(N != 0 || M != 0){
+        if(N != 0 && M != 0){
             printf("Ha ingresado N = %d y M = %d", N, M);
+
+            //Array with threads
+            pthread_t aThreads[N + M];
+
+            //Create type 1 thread
+            for (int i = 0; i < N; i++) {
+                pthread_t t;
+                if (pthread_create(&t, NULL, executeType1, 0) == 0){
+                    printf("No se ha podido crear el thread %d.", i);
+                } else {
+                    aThreads[i] = t;
+                }
+            }
+
+            //Create type 2 thread
+            for (int i = N; i < N + M; i++) {
+                pthread_t t;
+                if (pthread_create(&t, NULL, executeType2, 0) == 0){
+                    printf("No se ha podido crear el thread %d.", i);
+                } else {
+                    aThreads[i] = t;
+                }
+            }
+            
+            //Wait threads
+            for (int i = 0; i < N + M; i++){
+                pthread_join(aThreads[i], 0);
+            }
+
+            printf("Fin del programa.");
         } else {
             printf("Los números ingresados no son válidos.");
         }
